e3bd712b9bfce4e66fb531bb35635c9470a4ff3c
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-05-17 18:56:46 +0200

    *

--- Bucles/Ejercicio2.sh
+++ Bucles/Ejercicio2.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
+question=asd
+
 read -p "Crear archivo: " archivo
 touch $archivo
-echo "Se ha creado el archivo: " $archivo
+echo "Se ha creado el archivo: "$archivo
 sleep 3
 ls
-read -p "¿Desea borrar un archivo? " question
 
-if  [ "$question" = "si" ]; then
-        read -p "Nombre archivo: " nombre
-        read -p "Formato: " formato
-        echo "Archivo encontrado y borrado";
-        rm $nombre$formato
-else
-        echo "Ok, no borraremos ningún archivo";
-fi
+while [ "$question" != "si" ]; do
+    read -p "¿Desea borrar un archivo? " question
 
-#Hacer un script que:
-#Mediante un input podamos agregar un archivo (nos de un mensaje de confirmación)
-#Luego de 3 segundos de espera nos muestre la lista de archivos actual
-#Nos pregunte si deseamos borrar un archivo
-#De ser afirmativo, mediante un input podamos escribir por un lado el nombre del archivo, y por el otro  el formato
-#nos informe si el archivo fue encontrado y que lo borre
-#En el caso de que no queramos borrar ningun archivo, nos de un mensaje de “Ok, no borraremos ningún archivo”
+    if [ $question = "si" ]; then
+            read -p "Nombre archivo: " nombre
+            read -p "Formato: " formato
+            echo "Archivo encontrado y borrado";
+            rm $nombre$formato
+        elif [ $question = "no" ]; then
+            echo "Ok, no borraremos ningún archivo"
+            question=si
+        else
+            echo "$question no es una respuesta valida"
+    fi
+done
