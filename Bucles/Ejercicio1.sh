c367e2b5c1063f17447b5ddb7de647a61ff64a1a
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-05-17 16:26:38 +0200

    *

--- Bucles/Ejercicio1.sh
+++ Bucles/Ejercicio1.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+pid_actual=$$
+intentos=0
+valor=0
+
+while [ $valor -ne $pid_actual ]; do
+    read -p "Ingrese un valor: " valor
+
+    if [ $valor -gt $pid_actual ]; then
+            echo "Estas ingresando un valor mayor al PID actual"
+        elif [ $valor -ne $pid_actual ]; then
+            echo "Estas ingresando un valor menor al PID actual"
+        else
+            echo "¡Correcto!"
+    fi
+
+    intentos=$((intentos + 1)) 
+   
+done
+
+echo "¡Has acertado el PID actual! ... Número de intentos: $intentos"
