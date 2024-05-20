4ba8da958d8deb664d4fd2c129b872920d5fbef3
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-05-20 10:00:06 +0200

    *

--- Bucles/Ejercicio2.sh
+++ Bucles/Ejercicio2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-question=asd
+question=no
 
 read -p "Crear archivo: " archivo
 touch $archivo
@@ -9,14 +9,19 @@ sleep 3
 ls
 
 while [ "$question" != "si" ]; do
+    
     read -p "¿Desea borrar un archivo? (si/no) " question
 
-    if [ $question = "si" ]; then
+    if [ $question = si ]; then
             read -p "Nombre archivo: " nombre
             read -p "Formato: " formato
-            echo "Archivo encontrado y borrado";
-            rm $nombre$formato
-        elif [ $question = "no" ]; then
+            if [ -e $nombre$formato ]; then
+                    rm $nombre$formato
+                    echo "Archivo encontrado y borrado"
+                else
+                    echo "El archivo no existe"
+            fi
+        elif [ $question = no ]; then
             echo "Ok, no borraremos ningún archivo"
             question=si
         else
